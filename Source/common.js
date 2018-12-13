fc2b2135ac6a5a0e150331e262fefc90b747a6cf
Sindre Wilting <sindrewilting@gmail.com>
2018-12-13 13:00:37 +0100

    Simplify module

--- Source/common.js
+++ Source/common.js
@@ -1,27 +1,14 @@
-import { ArtifactTemplate } from './artifacts/ArtifactTemplate';
-import { BoilerPlate } from './boilerPlates/BoilerPlate';
-import { Cluster } from './configuration/Cluster';
-import { Config } from './configuration/Config';
-import { Dependency } from './dependencies/Dependency';
-import { Guid } from './Guid';
-
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
-module.exports.ArtifactTemplate = ArtifactTemplate;
-module.exports.BoilerPlate = BoilerPlate;
-module.exports.Cluster = Cluster;
-module.exports.Config = Config;
-module.exports.Dependency = Dependency;
-module.exports.Guid = Guid;
+import globals from './globals';
 
 export default {
     applications: require('./applications'),
     artifacts: require('./artifacts'),
     boundedContexts: require('./boundedContexts'),
     dependencies: require('./dependencies'),
-    globals: require('./globals').default,
+    globals: globals,
     helpers: require('./helpers')
 };
