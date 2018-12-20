3da33a166b6f8bc31829a0241ae01098b0efebbf
Sindre Wilting <sindrewilting@gmail.com>
2018-12-20 16:19:03 +0100

    common -> index

---
+++ Source/index.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export const globals = require('./globals').default;
+export const applications = require('./applications');
+export const artifacts = require('./artifacts');
+export const boundedContexts = require('./boundedContext');
+export const dependencies = require('./dependencies');
+export const helpers = require('./helpers');
+
