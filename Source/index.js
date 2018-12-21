0a4e8ef334f0fcd18e19af7b6e62cd806ea35443
Sindre Wilting <sindrewilting@gmail.com>
2018-12-21 18:53:28 +0100

    Export types

--- Source/index.js
+++ Source/index.js
@@ -3,6 +3,16 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 export * from './Guid';
+export * from './Folders';
+export * from './HttpWrapper';
+export * from './dependencies/Dependency';
+export * from './boilerPlates/BoilerPlate';
+export * from './boilerPlates/BoilerPlatesManager';
+export * from './artifacts/ArtifactTemplate';
+export * from './configuration/Config';
+export * from './configuration/Cluster';
+export * from './configuration/ConfigManager';
+export * from './configuration/ConfigParser';
 
 export const globals = require('./globals').default;
 export const applications = require('./applications');
