45fc4cd8bc7044123401a818cc7522c0327018e9
Sindre Wilting <sindrewilting@gmail.com>
2018-12-31 18:18:17 +0100

    Avoid using export *

--- Source/index.js
+++ Source/index.js
@@ -2,17 +2,17 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-export * from './Guid';
-export * from './Folders';
-export * from './HttpWrapper';
-export * from './dependencies/Dependency';
-export * from './boilerPlates/BoilerPlate';
-export * from './boilerPlates/BoilerPlatesManager';
-export * from './artifacts/ArtifactTemplate';
-export * from './configuration/Config';
-export * from './configuration/Cluster';
-export * from './configuration/ConfigManager';
-export * from './configuration/ConfigParser';
+export {Guid} from './Guid';
+export {Folders} from './Folders';
+export {HttpWrapper} from './HttpWrapper';
+export {dependencyFromJson, Dependency} from './dependencies/Dependency';
+export {BoilerPlate} from './boilerPlates/BoilerPlate';
+export {BoilerPlatesManager} from './boilerPlates/BoilerPlatesManager';
+export {artifactTemplateFromJson, ArtifactTemplate} from './artifacts/ArtifactTemplate';
+export {Config} from './configuration/Config';
+export {Cluster} from './configuration/Cluster';
+export {ConfigManager} from './configuration/ConfigManager';
+export {ConfigParser} from './configuration/ConfigParser';
 
 export const globals = require('./globals').default;
 export const applications = require('./applications');
