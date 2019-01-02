2cf9a2e279af5322565814da21454e5988ec8fc1
Sindre Wilting <sindrewilting@gmail.com>
2019-01-02 11:10:20 +0100

    specs for artifact template

---
+++ Source/artifacts/for_ArtifactTemplate/given/an_artifact_template_json.js
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+ export class an_artifact_template_json{
+    constructor() {
+        this.location = 'some location'; 
+        this.artifact_template_json = {
+            name: 'Aggregate Root template', 
+            type: 'aggregateRoot', 
+            area: 'domain', 
+            description: 'Creates an Aggregate Root', 
+            language: 'csharp', 
+            includedFiles: [
+                '{{name}}.cs'
+            ]
+        }; 
+    }
+ }
