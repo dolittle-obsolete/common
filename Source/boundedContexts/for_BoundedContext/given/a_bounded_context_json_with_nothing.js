1d96afa0dccf91a228499b1e1646bf309b0e6a26
Sindre Wilting <sindrewilting@gmail.com>
2019-01-03 12:21:23 +0100

    specs for bounded contexts

---
+++ Source/boundedContexts/for_BoundedContext/given/a_bounded_context_json_with_nothing.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export class a_bounded_context_json_with_nothing {
+    constructor() {
+        this.boundedContextJson = 
+            `{
+              }`;
+    }
+}
