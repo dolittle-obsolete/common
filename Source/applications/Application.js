8c0b8a46b9459cb436fca458611c90b97e614ac1
Sindre Wilting <sindrewilting@gmail.com>
2019-01-03 12:57:49 +0100

    Bug in Application

--- Source/applications/Application.js
+++ Source/applications/Application.js
@@ -22,7 +22,7 @@ export class Application {
     constructor(id, name, path) {
         this.#id = id;
         this.#name = name;
-        #this.#path = path;
+        this.#path = path;
     }
 
     /**
