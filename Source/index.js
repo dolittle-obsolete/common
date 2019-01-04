df9e892ce62b6d03e0a7358611efec1d09f12552
Sindre Wilting <sindrewilting@gmail.com>
2019-01-04 09:16:04 +0100

    Import and export stuff

--- Source/index.js
+++ Source/index.js
@@ -6,31 +6,36 @@ export const filesystem = require('fs-extra');
 import winston from 'winston';
 import simpleGit from 'simple-git';
 
-import {Folders} from './Folders';
-import {HttpWrapper} from './HttpWrapper';
-import {BoilerPlatesManager} from './boilerPlates/BoilerPlatesManager';
-import {ConfigManager} from './configuration/ConfigManager';
-import {ConfigParser} from './configuration/ConfigParser';
-import { ArtifactsManager } from './artifacts/ArtifactsManager';
 import { ApplicationsManager } from './applications/ApplicationsManager';
+import { ArtifactsManager } from './artifacts/ArtifactsManager';
+import { BoilerPlatesManager } from './boilerPlates/BoilerPlatesManager';
 import { BoundedContextsManager } from './boundedContexts/BoundedContextsManager';
+import { ConfigManager } from './configuration/ConfigManager';
+import { ConfigParser } from './configuration/ConfigParser';
 import { DependenciesManager } from './dependencies/DependenciesManager';
+import { Folders } from './Folders';
+import { HttpWrapper } from './HttpWrapper';
 
-export {Guid} from './Guid';
-export {Folders} from './Folders';
-export {HttpWrapper} from './HttpWrapper';
-export {Dependency} from './dependencies/Dependency';
+
+export {Application} from './applications/Application';
+export {ApplicationsManager} from './applications/ApplicationsManager';
+export { ArtifactsManager } from './artifacts/ArtifactsManager';
+export {ArtifactTemplate} from './artifacts/ArtifactTemplate';
 export {BoilerPlate} from './boilerPlates/BoilerPlate';
 export {BoilerPlatesManager} from './boilerPlates/BoilerPlatesManager';
-
-export {ArtifactTemplate} from './artifacts/ArtifactTemplate';
+export {BoundedContext} from './boundedContexts/BoundedContext';
+export {BoundedContextsManager} from './boundedContexts/BoundedContextsManager';
+export {Core} from './boundedContexts/Core'
 export {Config} from './configuration/Config';
 export {Cluster} from './configuration/Cluster';
 export {ConfigManager} from './configuration/ConfigManager';
 export {ConfigParser} from './configuration/ConfigParser';
-export { ArtifactsManager } from './artifacts/ArtifactsManager';
-export {ApplicationsManager} from './applications/ApplicationsManager';
 export { DependenciesManager } from './dependencies/DependenciesManager';
+export {Dependency} from './dependencies/Dependency';
+
+export {Folders} from './Folders';
+export {Guid} from './Guid';
+export {HttpWrapper} from './HttpWrapper';
 
 
 export const logger = winston.createLogger({
@@ -61,11 +66,12 @@ export const dolittleConfig = require('rc')('dolittle', {
         read: 'Read'
     }
 });
+
 export const folders = new Folders(filesystem);
 export const boilerPlatesManager = new BoilerPlatesManager(configManager, httpWrapper, git, folders, filesystem, logger);
-export const applicationsManager = new ApplicationsManager(boilerPlatesManager, logger);
+export const applicationsManager = new ApplicationsManager(boilerPlatesManager, filesystem, logger);
 export const artifactsManager = new ArtifactsManager(boilerPlatesManager, folders, filesystem, logger);
-export const boundedContextManager = new BoundedContextsManager(boilerPlatesManager, logger);
+export const boundedContextManager = new BoundedContextsManager(boilerPlatesManager, folders, filesystem, logger);
 export const dependenciesManager = new DependenciesManager(folders, filesystem, dolittleConfig, logger);
 
 export const helpers = require('./helpers');
