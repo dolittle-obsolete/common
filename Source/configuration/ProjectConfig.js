85b11fac5dc8488b18ac2449511b1b7b6b4610f5
Sindre Wilting <sindrewilting@gmail.com>
2019-02-19 07:13:47 +0100

    Introduce a cached-config

--- Source/configuration/ProjectConfig.js
+++ Source/configuration/ProjectConfig.js
@@ -2,9 +2,21 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import Conf from 'conf';
-export class ProjectConfig extends Conf {
+
+ import { CacheConfig } from './CacheConfig';
+
+/**
+ * Represents the tooling project cached project configuration file
+ *
+ * @export
+ * @class ProjectConfig
+ * @extends {CacheConfig}
+ */
+export class ProjectConfig extends CacheConfig {
     constructor() {
-        super({})
+        super('project', {
+            coreLanguage: 'csharp',
+            namespace: 'dolittle'
+        });
     }
 }
