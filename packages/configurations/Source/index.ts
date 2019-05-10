799a907a6d8e098e92d86e42ce0cb98eb9766ba4
Sindre Wilting <sindrewilting@gmail.com>
2019-05-10 17:17:07 +0200

    Non needed classes

--- packages/configurations/Source/index.ts
+++ packages/configurations/Source/index.ts
@@ -3,8 +3,4 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 export * from './CacheConfig';
-export * from './Cluster';
-export * from './ClusterConfig';
-export * from './ConfigManager';
-export * from './ConfigParser';
 export * from './ProjectConfig';
