1f9d3b758e493e9efc4385178f41b49919aa6078
Sindre Wilting <sindrewilting@gmail.com>
2019-05-12 17:09:09 +0200

    Introduce concept of IBoilerplatManagers

---
+++ packages/boilerplates/Source/BoilerplateManagers.ts
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { BaseBoilerplate } from './BaseBoilerplate';
+import { Boilerplate } from './Boilerplate';
+import { ICanManageBoilerplates } from './ICanManageBoilerplates';
+import { IBoilerplateManagers } from './IBoilerplateManagers';
+
+/**
+ * Represents the manager of boiler plates
+ */
+export class BoilerplateManagers implements IBoilerplateManagers {
+    
+    /**
+     * Creates an instance of BoilerplatesManager.
+     * @param {IBoilerplatesLoader} boilerplatesLoader
+     * @memberof BoilerplatesManager
+     */
+    constructor(managers: ICanManageBoilerplates[]) {
+        this.managers = managers;
+    } 
+    readonly managers: ICanManageBoilerplates[];
+    /**
+     * @inheritdoc
+     */
+    get boilerplates(): BaseBoilerplate[] {
+        let boilerplates: BaseBoilerplate[] = [];
+        this.managers.forEach(_ => boilerplates.push(..._.boilerplates));
+        return boilerplates;
+    }
+    /**
+     * @inheritdoc
+     */
+    boilerplatesByLanguage(language: string, namespace?: string): BaseBoilerplate[] {
+        return this.boilerplates.filter(_ => {
+            if (_.namespace) return _.namespace === namespace && _.language === language;
+            return _.language === language
+        });
+    }
+    /**
+     * @inheritdoc
+     */
+    boilerplatesByType(type: string, namespace?: string): BaseBoilerplate[] {
+        return this.boilerplates.filter(_ => {
+            if (_.namespace) return _.namespace === namespace && _.type === type;
+            return _.type === type;
+        });
+    }
+
+    /**
+     * @inheritdoc
+     */
+    boilerplatesByLanguageAndType(language: string, type: string, namespace?: string): BaseBoilerplate[] {
+        return this.boilerplates.filter(_ => {
+            if (_.namespace) return _.namespace === namespace && _.language == language && _.type == type;
+            return _.language == language && _.type == type;
+        });
+    }
+
+    /**
+     * @inheritdoc
+     * @memberof BoilerplatesManager
+     */
+    getAdornments(parentType: string, parentLanguage?: string, parentName?: string, namespace?: string): Boilerplate[] {
+        let boilerplates: Boilerplate[] = [];
+        this.boilerplates.forEach(_ => {
+            if (_ instanceof Boilerplate)
+                boilerplates.push(_);
+        });
+        
+        boilerplates =  boilerplates.filter(_ => {
+            if (_.namespace) return _.namespace === namespace && (_.parent && _.parent.type === parentType)
+            return _.parent && _.parent.type === parentType;
+        });
+        if (parentLanguage) boilerplates = boilerplates.filter(_ => {
+            if (_.parent.language) return _.parent.language === parentLanguage;
+            return true;
+        });
+        if (parentName) boilerplates = boilerplates.filter(_ => {
+            if (_.parent.name) return _.parent.name === parentName;
+            return true;
+        });
+
+        return boilerplates;
+    }
+    /**
+     * @inheritdoc
+     *
+     * @memberof BoilerplatesManager
+     */
+    getAdornmentsForBoilerplate(boilerplate: BaseBoilerplate, namespace?: string): Boilerplate[] {
+        return this.getAdornments(boilerplate.type, boilerplate.language, boilerplate.name, namespace);
+    }
+}
---
+++ packages/boilerplates/Source/IBoilerplateManagers.ts
@@ -0,0 +1,78 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Dolittle. All rights reserved.
+*  Licensed under the MIT License. See LICENSE in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { BaseBoilerplate } from "./BaseBoilerplate";
+import { Boilerplate } from "./Boilerplate";
+import { ICanManageBoilerplates } from "./ICanManageBoilerplates";
+/**
+ * Responsible for the boilerplate managers
+ *
+ * @export
+ * @interface IBoilerplateManagers
+ */
+export interface IBoilerplateManagers {
+    /**
+     * The boilerplate managers
+     *
+     * @type {ICanManageBoilerplates[]}
+     * @memberof IBoilerplateManagers
+     */
+    managers: ICanManageBoilerplates[] 
+    /**
+     * Get all boiler plates
+     *
+     * @type {BaseBoilerplate[]}
+     * @memberof IBoilerplateManagers
+     */
+    boilerplates: BaseBoilerplate[];
+
+    /**
+     * Get all available boiler plates for a specific language
+     * @param {string} language
+     * @param {string} [namespace=undefined]
+     * @returns {BaseBoilerplate[]} Available boiler plates for the language
+     * @memberof IBoilerplateManagers
+     */
+    boilerplatesByLanguage(language: string, namespace?: string): BaseBoilerplate[];
+
+    /**
+     * Get all available boiler plates for a specific type
+     * @param {string} type
+     * @param {string} [namespace=undefined]
+     * @returns {BaseBoilerplate[]} Available boiler plates for the type
+     * @memberof IBoilerplateManagers
+     */
+    boilerplatesByType(type: string, namespace?: string): BaseBoilerplate[];
+
+    /**
+     * Get all available boiler plates for a specific language
+     * @param {string} language
+     * @param {string} type
+     * @param {string} [namespace=undefined]
+     * @returns {BaseBoilerplate[]} Available boiler plates for the language
+     * @memberof IBoilerplateManagers
+     */
+    boilerplatesByLanguageAndType(language: string, type: string, namespace?: string): BaseBoilerplate[];
+
+     /**
+     * Gets the adornment boilerplates that has a parent with the given fields
+     *
+     * @param {string} parentType
+     * @param {string} [parentLanguage=undefined]
+     * @param {string} [parentName=undefined]
+     * @param {string} [namespace=undefined]
+     * @returns {Boilerplate[]}
+     * @memberof IBoilerplateManagers
+     */
+    getAdornments(parentType: string, parentLanguage?: string, parentName?: string, namespace?: string): Boilerplate[]
+     /**
+     * Gets the adornment boilerplates that has a parent with the given fields
+     *
+     * @param {BaseBoilerplate} boilerplate
+     * @returns {Boilerplate[]}
+     * @memberof IBoilerplateManagers
+     */
+    getAdornmentsForBoilerplate(boilerplate: BaseBoilerplate, namespace?: string): Boilerplate[]
+}
