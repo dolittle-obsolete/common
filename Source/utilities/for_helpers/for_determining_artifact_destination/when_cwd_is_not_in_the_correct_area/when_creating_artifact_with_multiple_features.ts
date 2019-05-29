ad202e4871c8318ba33b731477055458b15973bf
Sindre Wilting <sindrewilting@gmail.com>
2019-05-30 00:41:23 +0200

    Remove .given and .spec postfix

---
+++ Source/utilities/for_helpers/for_determining_artifact_destination/when_cwd_is_not_in_the_correct_area/when_creating_artifact_with_multiple_features.ts
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {determineDestination} from '../../../helpers';
+import { path_with_no_feature } from './given/a_path_with_no_feature';
+describe('when creating artifact with multiple features', () => {
+    const path = require('path');
+    let context = null;
+    let result = '';
+    let featureSegments = 'feature1.feature2';
+    (beforeEach => {
+        context = new path_with_no_feature();
+        result = determineDestination(context.area, context.language, `${featureSegments}.${context.name}`, context.cwd, context.boundedContextPath, context.dolittleConfig);
+    })();
+    it('Should determine the correct destination', () => result.destination.should.equal(path.join(context.boundedContextRoot, context.dolittleConfig[context.language][context.area], featureSegments.split('.').join(path.sep))));
+});
