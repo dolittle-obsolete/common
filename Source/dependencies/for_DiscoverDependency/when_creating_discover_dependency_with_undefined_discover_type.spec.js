1605d25a62dc76ff0786fc9141250d405c96afbc
Sindre Wilting <sindrewilting@gmail.com>
2019-05-29 10:35:17 +0200

    Add specs

---
+++ Source/dependencies/for_DiscoverDependency/when_creating_discover_dependency_with_undefined_discover_type.spec.js
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { DependencyMissingField, DiscoverDependency } from '../index';
+import { a_valid_configuration_for_discover_dependency } from './given/a_valid_configuration_for_discover_dependency.given';
+
+describe('when creating discover dependency with undefined discover type', () => {
+    let context = new a_valid_configuration_for_discover_dependency();
+    let discoverType = undefined;
+    let error = null;
+    try {
+        new DiscoverDependency(context.name, context.description, discoverType)
+    } catch(e) {
+        error = e;
+    }
+    it('Should throw an exception', () => error.should.not.be.null);
+    it('Should throw an DependencyMissingField exception', () => error.should.be.an.instanceof(Error))
+});
