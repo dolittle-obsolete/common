303ae67602b4f85efae2dcd42ca2a5247d603a7b
Sindre Wilting <sindrewilting@gmail.com>
2019-05-31 22:19:38 +0200

    Export Exception and increment minor version in 'utilities'

--- Source/utilities/index.ts
+++ Source/utilities/index.ts
@@ -3,4 +3,5 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 export * from './Guid';
+export * from './Exception';
 export * from './helpers';
