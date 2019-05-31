72918cceab20343610ee7ffc7839ee2a4e4180fd
Sindre Wilting <sindrewilting@gmail.com>
2019-05-31 22:21:05 +0200

    Extend Exception and increment minor version in 'packages'

--- Source/packages/connectivity/NotConnectedToInternet.ts
+++ Source/packages/connectivity/NotConnectedToInternet.ts
@@ -2,18 +2,19 @@
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
+import {Exception} from '@dolittle/tooling.common.utilities';
 
 /**
  * The exception that gets thrown when no connection to the internet could be established
  *
  * @export
  * @class NotConnectedToInternet
- * @extends {Error}
+ * @extends {Exception}
  */
-export class NotConnectedToInternet extends Error {
+export class NotConnectedToInternet extends Exception {
     /**
      * Instantiates an instance of {NotConnectedToInternet}.
-     * @param {...any} args
+     * @param {string} [message]
      */
     constructor(message?: string) {
         super(message? message : 'Could not establish an internet connection');
