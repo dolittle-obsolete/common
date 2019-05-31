10db0f888cc56bbb95e3d1701df9265a96f3983f
Sindre Wilting <sindrewilting@gmail.com>
2019-05-31 21:57:18 +0200

    Fix a spec in 'packages'

--- Source/packages/compatibility/for_packageVersionFunctions/for_is_greater_version/when_major_version_is_greater.ts
+++ Source/packages/compatibility/for_packageVersionFunctions/for_is_greater_version/when_major_version_is_greater.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { isGreaterVersion } from '../../compatibility/packageVersionFunctions';;
+import { isGreaterVersion } from '../../packageVersionFunctions';;
 describe('when major version is greater', () => {
     const lesserVersion = '1.0.0';
     const greaterVersion = '2.0.0';
