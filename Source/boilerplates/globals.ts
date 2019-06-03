7346e3b7d1e84667c7f317dc449f709e13879f72
Sindre Wilting <sindrewilting@gmail.com>
2019-06-03 22:20:45 +0200

    Fix exported globals

--- Source/boilerplates/globals.ts
+++ Source/boilerplates/globals.ts
@@ -7,12 +7,11 @@ import { dependencyParsers } from '@dolittle/tooling.common.dependencies';
 import { nodeModulesPath, toolingPackage, latestCompatiblePackageFinder } from '@dolittle/tooling.common.packages';
 import { fileSystem, folders } from '@dolittle/tooling.common.files';
 import { logger } from '@dolittle/tooling.common.logging';
-import {handlebars} from './toolingHandlebars';
 import {
     BoilerplatesConfig, IBoilerplatesLoader, BoilerplatesLoader, ICanDiscoverBoilerplates, BoilerplateDiscoverers, LocalBoilerplatesDiscoverer, IBoilerplateDiscoverers, 
     OnlineBoilerplatesDiscoverer, IApplicationsManager, ApplicationsManager, IBoundedContextsManager, BoundedContextsManager, 
     ITemplatesBoilerplates, TemplatesBoilerplates, IBoilerplateParsers, BoilerplateParsers, ICanParseBoilerplates, ContentBoilerplateParser, TemplatesBoilerplateParser,
-    Boilerplates, IBoilerplates, OnlineDolittleBoilerplatesFinder
+    Boilerplates, IBoilerplates, OnlineDolittleBoilerplatesFinder, handlebars, IContentBoilerplates, ContentBoilerplates
 } from './index';
 
 export const projectConfig = new ProjectConfig(nodeModulesPath);
@@ -31,15 +30,14 @@ let instancesOfICanDiscoverBoilerplates: ICanDiscoverBoilerplates[] = [
 
 export const boilerplateDiscoverers: IBoilerplateDiscoverers = new BoilerplateDiscoverers(instancesOfICanDiscoverBoilerplates);
 
-export const boilerplates: IBoilerplates = new Boilerplates(boilerplatesLoader, folders, fileSystem, logger, handlebars);
+export const boilerplates: IBoilerplates = new Boilerplates(boilerplatesLoader);
+export const templatesBoilerplates: ITemplatesBoilerplates = new TemplatesBoilerplates(boilerplatesLoader, folders, fileSystem, handlebars, logger);
+export const contentBoilerplates: IContentBoilerplates = new ContentBoilerplates(boilerplatesLoader, folders, fileSystem, logger, handlebars);
 
 export const onlineBoilerplatesFinder = new OnlineBoilerplatesDiscoverer(latestCompatiblePackageFinder, logger);
 
 export const onlineDolittleBoilerplatesFinder = new OnlineDolittleBoilerplatesFinder(latestCompatiblePackageFinder, logger);
 
-export const applicationsManager: IApplicationsManager = new ApplicationsManager(boilerplates, fileSystem, logger);
-
-export const artifactTemplates: IArtifactTemplates = new TemplatesBoilerplates(boilerplates, folders, fileSystem, Handlebars, logger);
-
-export const boundedContextsManager: IBoundedContextsManager = new BoundedContextsManager(boilerplates, applicationsManager, folders, fileSystem, logger);
+export const applicationsManager: IApplicationsManager = new ApplicationsManager(contentBoilerplates, fileSystem, logger);
+export const boundedContextsManager: IBoundedContextsManager = new BoundedContextsManager(contentBoilerplates, applicationsManager, folders, fileSystem, logger);
 
