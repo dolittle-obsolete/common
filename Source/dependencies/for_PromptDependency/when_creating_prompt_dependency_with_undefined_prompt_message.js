3caf31e10bdb3adc64fbb4c68397de5155dc7f69
Sindre Wilting <sindrewilting@gmail.com>
2019-06-04 09:25:40 +0200

    Fix some specs

--- Source/dependencies/for_PromptDependency/when_creating_prompt_dependency_with_undefined_prompt_message.js
+++ Source/dependencies/for_PromptDependency/when_creating_prompt_dependency_with_undefined_prompt_message.js
@@ -2,15 +2,15 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { DependencyMissingField, PromptDependency } from '../index';
+import { PromptDependency } from '../index';
 import { a_valid_configuration_for_a_prompt_dependency } from './given/a_valid_configuration_for_a_prompt_dependency';
 
-describe('when creating prompt dependency with undefined user input type', () => {
+describe('when creating prompt dependency with undefined prompt message', () => {
     let context = new a_valid_configuration_for_a_prompt_dependency();
-    let userInputType = undefined;
+    let promptMessage = undefined;
     let error = null;
     try {
-        let dep = new PromptDependency(context.name, context.description, userInputType, context.promptMessage);
+        let dep = new PromptDependency(context.name, context.description, context.userInputType, promptMessage);
         
     } catch(e) {
         error = e;
