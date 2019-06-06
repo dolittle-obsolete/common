a74d316ab0fb6599b337b1b4cfe0bbdba4ff889d
Sindre Wilting <sindrewilting@gmail.com>
2019-06-06 14:04:22 +0200

    Fix plugins listInstalledPlugins

--- Source/plugins/listInstalledPlugins.ts
+++ Source/plugins/listInstalledPlugins.ts
@@ -2,7 +2,7 @@
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
-import { OnStdCallback } from '@dolittle/tooling.common.packages';
+import { OnStdCallback, ToolingPackage } from '@dolittle/tooling.common.packages';
 import {FileSystem} from '@dolittle/tooling.common.files';
 import path from 'path';
 import { IPluginDiscoverers } from './index';
@@ -18,18 +18,20 @@ import { IPluginDiscoverers } from './index';
  * @export
  * @returns A list of the package configurations for each plugin
  */
+
+ 
 export async function listInstalledPlugins(pluginDiscoverers: IPluginDiscoverers, filesystem: FileSystem, onStdOut?: OnStdCallback, onNoPlugins?: OnStdCallback, onStdErr?: OnStdCallback) {
     let ifStdOut = (data: string) => onStdOut? onStdOut(data) : {};
     let ifNoPlugins = (data: string) => onNoPlugins? onNoPlugins(data) : {};
     let ifStdErr = (data: string) => onStdErr? onStdErr(data) : {};
     ifStdOut('Getting installed plugins:\n');
-    
+
     try {
         let paths = pluginDiscoverers.pluginPaths;
     
         let pluginPackages = paths.map(pluginPath => {
             let packageJson = filesystem.readJsonSync(path.join(pluginPath, 'package.json'));
-            return packageJson;
+            return packageJson as ToolingPackage;
         });
         let numPlugins = pluginPackages.length;
         if (numPlugins > 0) ifStdOut(`Found ${numPlugins} installed plugins`);
