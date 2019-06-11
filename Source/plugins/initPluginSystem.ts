efc541a6d5ecf7b99d9c71447c38b07e4ae7dcc8
Sindre Wilting <sindrewilting@gmail.com>
2019-06-11 06:50:27 +0200

    Provide providers to command manger when initializing plugin system

--- Source/plugins/initPluginSystem.ts
+++ Source/plugins/initPluginSystem.ts
@@ -3,6 +3,7 @@
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 import { IBusyIndicator } from '@dolittle/tooling.common.utilities';
+import { ICommandManager, ICanProvideDefaultCommands, ICanProvideDefaultCommandGroups, ICanProvideNamespaces } from '@dolittle/tooling.common.commands';
 import { IPlugins } from './index';
 
 /**
@@ -11,11 +12,22 @@ import { IPlugins } from './index';
  * @param {IPlugins} plugins
  * @param {IBusyIndicator} busyIndicator
  */
-export async function initPluginSystem(plugins: IPlugins, busyIndicator: IBusyIndicator) {
+export async function initPluginSystem(plugins: IPlugins, busyIndicator: IBusyIndicator, commandManager: ICommandManager) {
     busyIndicator = busyIndicator.createNew().start('Initializing plugin system');
     try {
         plugins.discoverNewPlugins();
         let loadedPlugins = await plugins.getPlugins();
+
+        commandManager.clear();
+        let providers: {command: ICanProvideDefaultCommands[], commandGroup: ICanProvideDefaultCommandGroups[], namespace: ICanProvideNamespaces[]} = {command: [], commandGroup: [], namespace: []};
+
+        loadedPlugins.forEach(_ => {
+            providers.command.push(_.defaultCommandsProvider);
+            providers.commandGroup.push(_.defaultCommandGroupsProvider);
+            providers.namespace.push(_.namespaceProvider);
+        });
+
+        commandManager.registerProviders(providers.command, providers.commandGroup, providers.namespace);
         
         busyIndicator.succeed('Plugin system initialized');
     } catch (error) {
