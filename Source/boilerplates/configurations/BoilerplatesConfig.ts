e61cfa681032e2e5fbb0c3b6bef6ce9543803311
Sindre Wilting <sindrewilting@gmail.com>
2019-08-27 17:29:45 +0200

    New release

--- Source/boilerplates/configurations/BoilerplatesConfig.ts
+++ Source/boilerplates/configurations/BoilerplatesConfig.ts
@@ -1,8 +1,8 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-import { GlobalCacheConfig } from '@dolittle/tooling.common.configurations';
+*  Copyright (c) Dolittle. All rights reserved.
+*  Licensed under the MIT License. See LICENSE in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+import { GlobalCacheConfig } from "@dolittle/tooling.common.configurations";
 
 /**
  * Represents the cached boilerplates configuration file for the tooling
