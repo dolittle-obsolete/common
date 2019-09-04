3ce5a203e1c9c1e3a6f30ea276f73ef8b1f0058d
Sindre Wilting <sindrewilting@gmail.com>
2019-09-04 18:58:30 +0200

    Remove unneeded dependency resolvers

--- Source/dependencies/globals.ts
+++ Source/dependencies/globals.ts
@@ -7,18 +7,14 @@ import { dolittleConfig } from '@dolittle/tooling.common.configurations';
 import { loggers } from '@dolittle/tooling.common.logging';
 import { 
     IDependencyDiscoverResolver, DependencyDiscoverResolver, IDependencyResolvers, DiscoverDependencyResolver, 
-    ICanResolveDependencies, DependencyResolvers, ICanParseDependencies, DiscoverDependencyParser, PromptDependencyParser, DiscoverAndPromptDependencyParser, IDependencyParsers, DependencyParsers, NonOptionalArgumentDependencyResolver, OptionalArgumentDependencyResolver 
+    ICanResolveDependencies, DependencyResolvers, ICanParseDependencies, DiscoverDependencyParser, PromptDependencyParser, DiscoverAndPromptDependencyParser, IDependencyParsers, DependencyParsers
 } from './index';
 
 export let dependencyDiscoverResolver: IDependencyDiscoverResolver = new DependencyDiscoverResolver(folders, fileSystem, dolittleConfig, loggers);
 
-export let nonOptionalArgumentDependencyResolver: ICanResolveDependencies = new NonOptionalArgumentDependencyResolver();
-
-export let optionalArgumentDependencyResolver: ICanResolveDependencies = new OptionalArgumentDependencyResolver();
-
 export let nonPromptDependencyResolver: ICanResolveDependencies = new DiscoverDependencyResolver(dependencyDiscoverResolver, dolittleConfig);
 
-let resolvers: ICanResolveDependencies[] = [nonOptionalArgumentDependencyResolver, optionalArgumentDependencyResolver, nonPromptDependencyResolver];
+let resolvers: ICanResolveDependencies[] = [nonPromptDependencyResolver];
 export let dependencyResolvers: IDependencyResolvers = new DependencyResolvers(resolvers, loggers);
 
 export let discoverDependencyParser: ICanParseDependencies = new DiscoverDependencyParser();
