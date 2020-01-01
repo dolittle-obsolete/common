333a696eba9ee4ecbf2c93fcb86cca348c2365e7
Sindre Wilting <sindrewilting@gmail.com>
2020-01-01 15:22:51 +0100

    lint fix

--- Source/host/NpmToolingPlatformDownloader.ts
+++ Source/host/NpmToolingPlatformDownloader.ts
@@ -2,7 +2,7 @@
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
-import { ICanDownloadToolingPlatform } from "./internal";
+import { ICanDownloadToolingPlatform } from './internal';
 import spawn from 'cross-spawn';
 import semver from 'semver';
 /**
@@ -12,7 +12,7 @@ import semver from 'semver';
  * @class NpmToolingPlatformDownloader
  */
 export class NpmToolingPlatformDownloader implements ICanDownloadToolingPlatform {
-    
+
     /**
      * Instantiates an instance of {NpmToolingPlatformDownloader.}
      * @param {string[]} _toolingPackages
@@ -20,9 +20,9 @@ export class NpmToolingPlatformDownloader implements ICanDownloadToolingPlatform
     constructor(private _toolingPackages: string[]) {}
 
     async download(toolingPackage: {version: string}) {
-        let child = spawn.sync('npm', ['i', '-g', ...this._toolingPackages.map(_ => `${_}@^${semver.major(toolingPackage.version)}.0.0`)], {cwd: process.cwd(), stdio: 'inherit'}); 
+        const child = spawn.sync('npm', ['i', '-g', ...this._toolingPackages.map(_ => `${_}@^${semver.major(toolingPackage.version)}.0.0`)], {cwd: process.cwd(), stdio: 'inherit'});
         if (child.error) throw child.error;
         if (child.status !== 0) throw new Error(`Could not download tooling platform version ${semver.major(toolingPackage.version)}`);
-        
+
     }
 }
