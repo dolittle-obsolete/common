8b675b2589f030829683ce050c8a2674f7ee5ff6
Sindre Wilting <sindrewilting@gmail.com>
2020-01-19 22:01:32 +0100

    Smart to build project before pushing

--- Source/common/globals.ts
+++ Source/common/globals.ts
@@ -3,14 +3,15 @@
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 import { IInitializer, Initializer, ProjectConfig } from './internal';
-import { boilerplates, boilerplatesLoader } from '@dolittle/tooling.common.boilerplates';
+import { boilerplates, boilerplatesLoader, boilerplateDiscoverers } from '@dolittle/tooling.common.boilerplates';
 import { commandManager, providerRegistrators, ICanRegisterProviders } from '@dolittle/tooling.common.commands';
-import { plugins } from '@dolittle/tooling.common.plugins';
+import { plugins, onlineDolittlePluginsFinder } from '@dolittle/tooling.common.plugins';
 import { loggers } from '@dolittle/tooling.common.logging';
 import { ProviderRegistrator } from './internal';
 import { contexts } from '@dolittle/tooling.common.login';
+import { connectionChecker, npmPackageDownloader } from '@dolittle/tooling.common.packages';
 
-export let initializer: IInitializer = new Initializer(providerRegistrators, commandManager, plugins, boilerplates, boilerplatesLoader, loggers);
+export let initializer: IInitializer = new Initializer(providerRegistrators, commandManager, plugins, boilerplates, boilerplatesLoader, boilerplateDiscoverers, onlineDolittlePluginsFinder, connectionChecker, npmPackageDownloader, loggers);
 
 const providerRegistrator: ICanRegisterProviders = new ProviderRegistrator(commandManager, initializer, contexts, loggers);
 providerRegistrators.addRegistrators(providerRegistrator);
